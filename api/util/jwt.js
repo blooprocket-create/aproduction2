fc289b0e237c09777d6e2ddcbc6a341eb68a8280
blooprocket-create <blooprocket@gmail.com>
2025-09-29 17:46:50 -0400

    Delete api directory

--- api/util/jwt.js
+++
@@ -1,6 +0,0 @@
-
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.JWT_SECRET || 'dev-secret-change-me';
-const sign = (payload, opts={}) => jwt.sign(payload, SECRET, { expiresIn: '7d', ...opts });
-const verify = (token) => { try { return jwt.verify(token, SECRET); } catch { return null; } };
-module.exports = { sign, verify };
